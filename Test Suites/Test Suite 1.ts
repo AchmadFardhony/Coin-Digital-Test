787bcc31e0c9a57d1e8388c026cd0f74676236e3
ACHMAD FARDHONY <Achmadfardhony@gmail.com>
2023-01-18 08:57:28 +0700

    Test Pertama

---
+++ Test Suites/Test Suite 1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dfc03706-8a19-4f45-bdae-a22ce170db6d</testSuiteGuid>
+</TestSuiteEntity>
